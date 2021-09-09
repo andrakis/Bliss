bede333747d9f2973a1441d7fa46445c63fa9a65
Julian Thatcher <daedalus.raistlin@gmail.com>
2021-09-09 14:12:40 +1000

    Reorder and reformat to more logical layout

--- Bliss/Bliss.cpp
+++ Bliss/Bliss.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "Bliss.h"
 
+// Shared
 namespace Bliss {
 	BVarAtomLibrary AtomLibrary;
 	BVar True(0), False(0), Nil(0); // Filled in later
@@ -15,68 +16,118 @@ namespace Bliss {
 		Nil = BVar::Atom("nil");
 		return 0;
 	}
+}
 
+// BVar
+namespace Bliss {
+	using namespace Containers;
 
-	BVar BVarContainer::head() const { return Nil; }
-	BVar BVarContainer::tail() const { return Nil; }
-
-	BVarContainer *BVarIntContainer::duplicate() const {
-		return new BVarIntContainer(value);
+	BVar::BVar(int value) : container(new BVarIntContainer(value)) { }
+	BVar::BVar(std::string value) : container(new BVarStringContainer(value)) { }
+	BVar::BVar(BVarContainer *cnt) : container(cnt) { }
+	BVar::BVar(BVarContainer const &cnt) : container(cnt.duplicate()) { }
+	BVar::BVar(BVar const &other) : container(other.container->duplicate()) { }
+	BVar BVar::Atom(std::string name) {
+		return BVar(new BVarAtomContainer(name));
 	}
-	BVarContainer *BVarStringContainer::duplicate() const {
-		return new BVarStringContainer(value);
-	}
-	BVarContainer *BVarAtomContainer::duplicate() const {
-		return new BVarAtomContainer(*this);
-	}
-	BVarContainer *BVarListContainer::duplicate() const {
-		return new BVarListContainer(*this);
-	}
-
 	BVar BVar::List() {
 		return BVar(new BVarListContainer());
 	}
 	BVar BVar::List(const ListType &l) {
 		return BVar(new BVarListContainer(l));
 	}
+	BVar::~BVar() { delete container; }
+	int BVar::IntValue() const { return container->IntValue(); }
+	std::string BVar::StringValue() const { return container->StringValue(); }
+	std::string BVar::StringRepr() const { return container->StringRepr(); }
+	bool BVar::CompEq (const BVar &other) const { return container->CompEq(*other.container); }
+	void BVar::assign(BVarContainer* cnt) {
+		free(container);
+		container = cnt->duplicate();
+	}
+	BVar &BVar::operator+= (const BVar &rhs)& throw(BInvalidOperationException) {
+		container->add(*rhs.container);
+		return *this;
+	}
+	BVar &BVar::operator-= (const BVar &rhs)& throw (BInvalidOperationException) {
+		container->subtract(*rhs.container);
+		return *this;
+	} 
+	bool BVar::operator== (const BVar &rhs) { return container->CompEq(*rhs.container); }
+	bool BVar::operator!= (const BVar &rhs) { return !(operator==(rhs)); }
+	std::ostream &operator<<(std::ostream &os, const BVar &v)
+	{
+		os << v.StringValue();
+		return os;
+	}
+	BVar &BVar::operator=(const BVar &&other) noexcept {
+		BVar tmp(other);
+		std::swap(container, tmp.container);
+		return *this;
+	}
+	BVar &BVar::operator=(BVar &other) noexcept {
+		std::swap(container, other.container);
+		return *this;
+	}
+
+	// BVarContainer
+	BVar BVarContainer::head() const { return Nil; }
+	BVar BVarContainer::tail() const { return Nil; }
+
 	BVar BVarContainer::index(size_t index) const
 	{
 		return Nil;
 	}
 
-	bool BVarListContainer::CompEq(const BVarContainer &other) const {
-		if (other.type != BVarType::List)
-			return false;
-		const BVarListContainer *lc = dynamic_cast<const BVarListContainer*>(&other);
-		if (!lc)
-			return false;
-		auto it1 = value.cbegin(), it2 = lc->value.cbegin();
-		for (; 
-			it1 != value.cend() && it2 != lc->value.cend();
-			++it1, ++it2) {
-			if (!it1->CompEq(*it2))
-				return false;
+	// Subcontainers
+	namespace Containers {
+		BVarContainer *BVarIntContainer::duplicate() const {
+			return new BVarIntContainer(value);
 		}
-		return it1 == value.cend() && it2 == lc->value.cend();
-	}
+		BVarContainer *BVarStringContainer::duplicate() const {
+			return new BVarStringContainer(value);
+		}
+		BVarContainer *BVarAtomContainer::duplicate() const {
+			return new BVarAtomContainer(*this);
+		}
+		BVarContainer *BVarListContainer::duplicate() const {
+			return new BVarListContainer(*this);
+		}
 
-	BVar BVarStringContainer::index(size_t index) const {
-		auto it = value.cbegin() + index;
-		if (it != value.cend())
-			return BVar(std::string(value, index, 1));
-		return Nil;
-	}
-	BVar BVarStringContainer::head() const { 
-		auto it = value.cbegin();
-		if (it != value.cend())
-			return BVar(std::string(1, *it));
-		return Nil;
-	}
-	BVar BVarStringContainer::tail() const {
-		auto it = value.crbegin();
-		if (it != value.crend())
-			return BVar(std::string(1, *it));
-		return Nil;
+		bool BVarListContainer::CompEq(const BVarContainer &other) const {
+			if (other.type != BVarType::List)
+				return false;
+			const BVarListContainer *lc = dynamic_cast<const BVarListContainer*>(&other);
+			if (!lc)
+				return false;
+			auto it1 = value.cbegin(), it2 = lc->value.cbegin();
+			for (; 
+				it1 != value.cend() && it2 != lc->value.cend();
+				++it1, ++it2) {
+				if (!it1->CompEq(*it2))
+					return false;
+			}
+			return it1 == value.cend() && it2 == lc->value.cend();
+		}
+
+		BVar BVarStringContainer::index(size_t index) const {
+			auto it = value.cbegin() + index;
+			if (it != value.cend())
+				return BVar(std::string(value, index, 1));
+			return Nil;
+		}
+		BVar BVarStringContainer::head() const { 
+			auto it = value.cbegin();
+			if (it != value.cend())
+				return BVar(std::string(1, *it));
+			return Nil;
+		}
+		BVar BVarStringContainer::tail() const {
+			auto it = value.crbegin();
+			if (it != value.crend())
+				return BVar(std::string(1, *it));
+			return Nil;
+		}
 	}
 }
 
--- Bliss/Bliss.h
+++ Bliss/Bliss.h
@@ -34,6 +34,35 @@ namespace Bliss {
 		std::string message;
 	};
 
+	class BVarContainer;
+	typedef typename std::deque<BVar> ListType;
+	class BVar {
+		BVarContainer* container;
+	public:
+		BVar(int value);
+		BVar(std::string value);
+		BVar(BVarContainer *cnt);
+		BVar(BVarContainer const &cnt);
+		BVar(BVar const &other);
+		static BVar Atom(std::string name);
+		static BVar List();
+		static BVar List(const ListType &l);
+		~BVar();
+		int IntValue() const;
+		std::string StringValue() const;
+		std::string StringRepr() const;
+		bool CompEq (const BVar &other) const;
+		void assign(BVarContainer* cnt);
+		BVar &operator+= (const BVar &rhs)& throw(BInvalidOperationException);
+		BVar &operator-= (const BVar &rhs)& throw (BInvalidOperationException);
+		bool operator== (const BVar &rhs);
+		bool operator!= (const BVar &rhs);
+		BVar &operator=(const BVar &&other) noexcept;
+		BVar &operator=(BVar &other) noexcept;
+	};
+
+	std::ostream &operator<<(std::ostream &os, const BVar &v);
+
 	class BVarContainer {
 	protected:
 		BVarContainer(BVarType t) : type(t) { }
@@ -66,70 +95,6 @@ namespace Bliss {
 		virtual BVar tail() const;
 	};
 
-	class BVarIntContainer : public BVarContainer {
-	protected:
-		int value;
-	public:
-		BVarIntContainer(int v) : BVarContainer(BVarType::Integer), value(v) { }
-		int IntValue() const { return value; }
-		std::string StringValue() const { return std::to_string(value); }
-		std::string StringRepr() const { return StringValue(); }
-		BVarContainer* duplicate() const;
-		void add(const BVarContainer &other) throw(BInvalidOperationException) {
-			// TODO: type checking
-			if (!other.CanCastInt())
-				throw new BInvalidOperationException();
-			value += other.IntValue();
-		}
-		void subtract(const BVarContainer &other) throw(BInvalidOperationException) {
-			// TODO: type checking
-			if (!other.CanCastInt())
-				throw new BInvalidOperationException();
-			value -= other.IntValue();
-		}
-		void multiply(const BVarContainer &other) throw(BInvalidOperationException) {
-			// TODO: type checking
-			if (!other.CanCastInt())
-				throw new BInvalidOperationException();
-			value *= other.IntValue();
-		}
-		void divide(const BVarContainer &other) throw(BInvalidOperationException) {
-			// TODO: type checking
-			if (!other.CanCastInt())
-				throw new BInvalidOperationException();
-			value /= other.IntValue();
-		}
-		bool CompEq(const BVarContainer &other) const {
-			return value == other.IntValue();
-		}
-		bool CanCastInt() const { return true; }
-		bool CanCastString() const { return true; }
-	};
-
-	class BVarStringContainer : public BVarContainer {
-	protected:
-		std::string value;
-	public:
-		BVarStringContainer(std::string v) : BVarContainer(BVarType::String), value(v) { }
-		int IntValue() const { return std::stoi(value); }
-		std::string StringValue() const { return value; }
-		std::string StringRepr() const { return "\"" + value + "\""; }
-		BVarContainer* duplicate() const;
-		void add(const BVarContainer &other) throw(BInvalidOperationException) {
-			// TODO: type checking
-			if (!other.CanCastString())
-				throw new BInvalidOperationException();
-			value += other.StringValue();
-		}
-		bool CompEq(const BVarContainer &other) const {
-			return value == other.StringValue();
-		}
-		bool CanCastInt() const { return true; }
-		bool CanCastString() const { return true; }
-		virtual BVar index(size_t index) const;
-		virtual BVar head() const;
-		virtual BVar tail() const;
-	};
 
 	class BVarAtomLibrary {
 	public:
@@ -159,131 +124,146 @@ namespace Bliss {
 			return it->second;
 		}
 	};
+
 	extern BVarAtomLibrary AtomLibrary;
-	class BVarAtomContainer : public BVarContainer {
-		std::string name;
-		int value;
-	public:
-		BVarAtomContainer(std::string n) : BVarContainer(BVarType::Atom),
-			value(AtomLibrary.declare(n)), name(n) { }
-		BVarAtomContainer(const BVarAtomContainer &other) : BVarContainer(BVarType::Atom),
-			value(other.value), name(other.name) { }
-		int IntValue() const { return value; }
-		std::string StringValue() const { return name; }
-		std::string StringRepr() const { return "'" + name; }
-		bool CompEq(const BVarContainer &other) const {
-			if (other.type != BVarType::Atom)
-				return false;
-			return value == other.IntValue();
-		}
 
-		BVarContainer* duplicate() const;
-		bool CanCastInt() const { return false; }
-		bool CanCastString() const { return false; }
-	};
-
-	class BVar;
-	typedef typename std::deque<BVar> ListType;
-	class BVar {
-		BVarContainer* container;
-	public:
-		BVar(int value) : container(new BVarIntContainer(value)) { }
-		BVar(std::string value) : container(new BVarStringContainer(value)) { }
-		BVar(BVarContainer *cnt) : container(cnt) { }
-		BVar(BVarContainer const &cnt) : container(cnt.duplicate()) { }
-		BVar(BVar const &other) : container(other.container->duplicate()) { }
-		static BVar Atom(std::string name) {
-			return BVar(new BVarAtomContainer(name));
-		}
-		static BVar List();
-		static BVar List(const ListType &l);
-		~BVar() { delete container; }
-		int IntValue() const { return container->IntValue(); }
-		std::string StringValue() const { return container->StringValue(); }
-		std::string StringRepr() const { return container->StringRepr(); }
-		bool CompEq (const BVar &other) const { return container->CompEq(*other.container); }
-		void assign(BVarContainer* cnt) {
-			free(container);
-			container = cnt->duplicate();
-		}
-		BVar &operator+= (const BVar &rhs)& throw(BInvalidOperationException) {
-			container->add(*rhs.container);
-			return *this;
-		}
-		BVar &operator-= (const BVar &rhs)& throw (BInvalidOperationException) {
-			container->subtract(*rhs.container);
-			return *this;
-		}
-		bool operator== (const BVar &rhs) {
-			return container->CompEq(*rhs.container);
-		}
-		bool operator!= (const BVar &rhs) {
-			return !(operator==(rhs));
-		}
-		friend std::ostream &operator<<(std::ostream &os, const BVar &v) {
-			os << v.container->StringValue();
-			return os;
-		}
-		BVar &operator=(const BVar &&other) noexcept {
-			BVar tmp(other);
-			std::swap(container, tmp.container);
-			return *this;
-		}
-		BVar &operator=(BVar &other) noexcept {
-			std::swap(container, other.container);
-			return *this;
-		}
-	};
-
-	class BVarListContainer : public BVarContainer {
-	public:
-		typedef typename ListType::iterator iterator;
-		typedef typename ListType::const_iterator const_iterator;
-	private:
-		ListType value;
-		std::string StringMap(bool repr = false) const {
-			std::string result = "(";
-			for(auto it = value.cbegin(); it != value.cend(); ++it) {
-				if (it != value.cbegin())
-					result += " ";
-				result += repr ? it->StringRepr() : it->StringValue();
+	namespace Containers {
+		class BVarIntContainer : public BVarContainer {
+		protected:
+			int value;
+		public:
+			BVarIntContainer(int v) : BVarContainer(BVarType::Integer), value(v) { }
+			int IntValue() const { return value; }
+			std::string StringValue() const { return std::to_string(value); }
+			std::string StringRepr() const { return StringValue(); }
+			BVarContainer* duplicate() const;
+			void add(const BVarContainer &other) throw(BInvalidOperationException) {
+				// TODO: type checking
+				if (!other.CanCastInt())
+					throw new BInvalidOperationException();
+				value += other.IntValue();
 			}
-			result += ")";
-			return result;
-		}
-	public:
-		BVarListContainer() : BVarContainer(BVarType::List), value() { }
-		BVarListContainer(ListType l) : BVarContainer(BVarType::List), value(l) { }
-		void add(const BVarContainer &other) throw(BInvalidOperationException) {
-			value.push_back(BVar(other.duplicate()));
-		}
+			void subtract(const BVarContainer &other) throw(BInvalidOperationException) {
+				// TODO: type checking
+				if (!other.CanCastInt())
+					throw new BInvalidOperationException();
+				value -= other.IntValue();
+			}
+			void multiply(const BVarContainer &other) throw(BInvalidOperationException) {
+				// TODO: type checking
+				if (!other.CanCastInt())
+					throw new BInvalidOperationException();
+				value *= other.IntValue();
+			}
+			void divide(const BVarContainer &other) throw(BInvalidOperationException) {
+				// TODO: type checking
+				if (!other.CanCastInt())
+					throw new BInvalidOperationException();
+				value /= other.IntValue();
+			}
+			bool CompEq(const BVarContainer &other) const {
+				return value == other.IntValue();
+			}
+			bool CanCastInt() const { return true; }
+			bool CanCastString() const { return true; }
+		};
 
-		int IntValue() const { return 0; }
-		std::string StringValue() const { return StringMap(); }
-		std::string StringRepr() const { return StringMap(true); }
-		BVarContainer* duplicate() const;
-		bool CompEq(const BVarContainer &other) const;
-		bool CanCastInt() const { return false; }
-		bool CanCastString() const { return false; }
+		class BVarStringContainer : public BVarContainer {
+		protected:
+			std::string value;
+		public:
+			BVarStringContainer(std::string v) : BVarContainer(BVarType::String), value(v) { }
+			int IntValue() const { return std::stoi(value); }
+			std::string StringValue() const { return value; }
+			std::string StringRepr() const { return "\"" + value + "\""; }
+			BVarContainer* duplicate() const;
+			void add(const BVarContainer &other) throw(BInvalidOperationException) {
+				// TODO: type checking
+				if (!other.CanCastString())
+					throw new BInvalidOperationException();
+				value += other.StringValue();
+			}
+			bool CompEq(const BVarContainer &other) const {
+				return value == other.StringValue();
+			}
+			bool CanCastInt() const { return true; }
+			bool CanCastString() const { return true; }
+			virtual BVar index(size_t index) const;
+			virtual BVar head() const;
+			virtual BVar tail() const;
+		};
 
-		BVar index(size_t index) const {
-			auto it = value.cbegin() + index;
-			if (it != value.cend())
-				return *it;
-			return Nil;
-		}
-		BVar head() const { 
-			auto it = value.cbegin();
-			if (it != value.cend())
-				return *it;
-			return Nil;
-		}
-		BVar tail() const {
-			auto it = value.crbegin();
-			if (it != value.crend())
-				return *it;
-			return Nil;
-		}
-	};
+		class BVarAtomContainer : public BVarContainer {
+			std::string name;
+			int value;
+		public:
+			BVarAtomContainer(std::string n) : BVarContainer(BVarType::Atom),
+				value(AtomLibrary.declare(n)), name(n) { }
+			BVarAtomContainer(const BVarAtomContainer &other) : BVarContainer(BVarType::Atom),
+				value(other.value), name(other.name) { }
+			int IntValue() const { return value; }
+			std::string StringValue() const { return name; }
+			std::string StringRepr() const { return "'" + name; }
+			bool CompEq(const BVarContainer &other) const {
+				if (other.type != BVarType::Atom)
+					return false;
+				return value == other.IntValue();
+			}
 
+			BVarContainer* duplicate() const;
+			bool CanCastInt() const { return false; }
+			bool CanCastString() const { return false; }
+		};
+
+		class BVarListContainer : public BVarContainer {
+		public:
+			typedef typename ListType::iterator iterator;
+			typedef typename ListType::const_iterator const_iterator;
+		private:
+			ListType value;
+			std::string StringMap(bool repr = false) const {
+				std::string result = "(";
+				for(auto it = value.cbegin(); it != value.cend(); ++it) {
+					if (it != value.cbegin())
+						result += " ";
+					result += repr ? it->StringRepr() : it->StringValue();
+				}
+				result += ")";
+				return result;
+			}
+		public:
+			BVarListContainer() : BVarContainer(BVarType::List), value() { }
+			BVarListContainer(ListType l) : BVarContainer(BVarType::List), value(l) { }
+			void add(const BVarContainer &other) throw(BInvalidOperationException) {
+				value.push_back(BVar(other.duplicate()));
+			}
+
+			int IntValue() const { return 0; }
+			std::string StringValue() const { return StringMap(); }
+			std::string StringRepr() const { return StringMap(true); }
+			BVarContainer* duplicate() const;
+			bool CompEq(const BVarContainer &other) const;
+			bool CanCastInt() const { return false; }
+			bool CanCastString() const { return false; }
+
+			BVar index(size_t index) const {
+				auto it = value.cbegin() + index;
+				if (it != value.cend())
+					return *it;
+				return Nil;
+			}
+			BVar head() const { 
+				auto it = value.cbegin();
+				if (it != value.cend())
+					return *it;
+				return Nil;
+			}
+			BVar tail() const {
+				auto it = value.crbegin();
+				if (it != value.crend())
+					return *it;
+				return Nil;
+			}
+		};
+	}
 }
